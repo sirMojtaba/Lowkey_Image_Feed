0a02c6c07909ee50f3b8de5b61b1316117a91505
m.hassanzadeh <m.hassanzadeh@kianiranian.com>
2024-03-16 23:42:35 +0330

    Implement PhotosAdapter

---
+++ app/src/main/java/com/example/lowkeyimagefeed/ui/adapter/PhotosAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.lowkeyimagefeed.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.lowkeyimagefeed.databinding.ItemImageBinding
+import com.example.lowkeyimagefeed.domain.Photo
+
+class PhotosAdapter : RecyclerView.Adapter<PhotosAdapter.PhotoViewHolder>() {
+
+    private val photos: List<Photo> = emptyList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+        val itemBinding =
+            ItemImageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PhotoViewHolder(itemBinding)
+    }
+
+    override fun getItemCount(): Int {
+        return photos.size
+    }
+
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+        val photo = photos[position]
+        holder.bind(photo)
+    }
+
+    class PhotoViewHolder(private val binding: ItemImageBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(photo: Photo) {
+            binding.authorsName.text = photo.photographer
+            Glide.with(binding.root).load(photo.src.medium).into(binding.iv)
+        }
+    }
+}
