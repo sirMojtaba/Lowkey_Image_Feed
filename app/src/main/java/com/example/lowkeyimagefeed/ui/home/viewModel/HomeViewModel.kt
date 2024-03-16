4d33ca821d3630417002594207663a0a56576e84
m.hassanzadeh <m.hassanzadeh@kianiranian.com>
2024-03-16 23:00:41 +0330

    Implement getPhotos in HomeViewModel

--- app/src/main/java/com/example/lowkeyimagefeed/ui/home/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/lowkeyimagefeed/ui/home/viewModel/HomeViewModel.kt
@@ -1,7 +1,29 @@
 package com.example.lowkeyimagefeed.ui.home.viewModel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.lowkeyimagefeed.domain.PhotosResponse
+import com.example.lowkeyimagefeed.domain.RequestPhotos
+import com.example.lowkeyimagefeed.usecase.GetPhotosInteractor
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val getPhotosInteractor: GetPhotosInteractor
+) : ViewModel() {
+
+    private val _photos: MutableLiveData<PhotosResponse> = MutableLiveData()
+    val photos: LiveData<PhotosResponse> = _photos
+
+    fun getPhotos(requestPhotos: RequestPhotos) {
+        viewModelScope.launch {
+            getPhotosInteractor.execute(requestPhotos).collect {
+                _photos.value = it
+            }
+        }
+    }
 }
