56d3497bde5722c4c4a9b1cb1ac152b46bcb0094
m.hassanzadeh <m.hassanzadeh@kianiranian.com>
2024-03-16 21:13:40 +0330

    Create home fragment with recycler view item

---
+++ app/src/main/java/com/example/lowkeyimagefeed/ui/home/view/HomeFragment.kt
@@ -0,0 +1,27 @@
+package com.example.lowkeyimagefeed.ui.home.view
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.lowkeyimagefeed.R
+import com.example.lowkeyimagefeed.ui.home.viewModel.HomeViewModel
+
+class HomeFragment : Fragment() {
+
+    private val viewModel: HomeViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
